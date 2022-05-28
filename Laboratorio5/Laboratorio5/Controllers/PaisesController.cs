534060ab32ddd9a48d85963cabaf0babd61169ac
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-05-28 09:46:01 -0600

    Added methods to create the country and bind the view of the form

--- Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
+++ Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
@@ -1,4 +1,5 @@
 ﻿using Laboratorio5.Handlers;
+using Laboratorio5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,42 @@ namespace Laboratorio5.Controllers
             PaisesHandler paisesHandler = new PaisesHandler();
             var paises = paisesHandler.ObtenerPaises();
             ViewBag.MaintTitle = "Lista de paises";
-            return View(paises);
+            return View(paises);    
+        }
 
-           
+
+
+        [HttpGet]
+        public ActionResult CrearPais()
+        {
+            return View;
+        }
+
+        [HttpPost]
+        public ActionResult CrearPais(PaisModel pais)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PaisesHandler paisesHandler = new PaisesHandler();
+                    ViewBag.ExitoAlCrear = paisesHandler.CrearPais(pais);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El país" + " " + pais.Nombre + " fue creado con éxito";
+                        ModelState.Clear();
+                    }
+
+
+                }
+                return View();
+            }
+            catch 
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el país";
+                return View();
+            }
         }
     }
 }
